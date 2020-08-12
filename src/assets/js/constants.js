26abe6eae4fbd4f9973d9016dcfb3cb9e474ae39
chenmingzhen <214164051@qq.com>
2020-08-12 16:18:46 +0800

    完成歌手页面点击更多  点击歌详情的数值滚动到指定位置

--- src/assets/js/constants.js
+++ src/assets/js/constants.js
@@ -1,4 +1,4 @@
 //content
 export const GET_BANNER = "content/GET_BANNER";
 
-export const SHARED_URL = "http://http://193.112.175.198/Ethan/#/shared?";
+export const SHARED_URL = "http://193.112.175.198/Ethan/#/shared?";
